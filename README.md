2511b3c9b7cb2826bfe2f9d12880e227c552680a
Michael R. Maletich <mmaletich@shoprunner.com>
2018-06-20 15:28:04 -0500

    Initial Checkin.

---
+++ README.md
@@ -0,0 +1,17 @@
+# ShopRunner Example SOAP Client
+
+This is an example of how to send an order to ShopRunner SOAP API in Python
+
+# Setup
+This client requires Python 3 be installed.
+
+Install Dependencies:
+```
+pip install -r requirements.txt
+```
+
+# Send an order to the staging environment
+
+```
+./send_test_order_to_staging.py <username> <password>
+```
---
+++ send_test_order_to_staging.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+import argparse
+from zeep import Client
+from zeep.wsse.username import UsernameToken
+import logging.config
+
+logging.config.dictConfig({
+    'version': 1,
+    'formatters': {
+        'verbose': {
+            'format': '%(name)s: %(message)s'
+        }
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose',
+        },
+    },
+    'loggers': {
+        'zeep.transports': {
+            'level': 'DEBUG',
+            'propagate': True,
+            'handlers': ['console'],
+        },
+    }
+})
+
+parser = argparse.ArgumentParser(description='ShopRunner SOAP Example')
+parser.add_argument('username', help='ShopRunner Staging Username')
+parser.add_argument('password', help='ShopRunner Staging Password')
+
+args = parser.parse_args()
+
+wsdl_url = 'https://services.shoprunner.com/staging/services/order?wsdl'
+client = Client(
+    wsdl_url,
+    wsse=UsernameToken(args.username, args.password, use_digest=True))
+
+order_request_type = client.get_type('ns0:OrderRequestType')
+order_type = client.get_type('ns0:Order')
+
+results = client.service.Order(
+    Partner="TESTPARTNER",
+    Order=[
+        order_type(
+            OrderNumber="TEST100",
+            OrderDate="2014-01-10T16:58:45",
+            SRAuthenticationToken="037t4ufg820r3ge87rgf9r3x",
+            CurrencyCode="USD",
+            TotalNumberOfItems=1,
+            TotalNumberOfShopRunnerItems=1,
+            OrderTax=0.0,
+            OrderTotal=205.68)
+    ])
+
+print(results)
